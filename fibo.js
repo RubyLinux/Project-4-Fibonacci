d9202bb49a563f2ab80ad83cbd617d5dd51427f8
RubyLinux <linuxlucifer@gmail.com>
2016-09-25 00:50:59 +0100

    Update fibo.js

--- fibo.js
+++ fibo.js
@@ -1,15 +1,16 @@
-var c = new fibonacci();
-			var fiboSeq = "";
-				for ( var i = 2 ; i <= 100 ; i++ ) {
-					fiboSeq += c.looping(i) + " ";}
+function fibonacci() {
+	this.looping = function(nb){
+		var a = 0
+		var b = 1
+		var c = null;
+			for ( var i = 2; i <= nb; i++){
+				c = a + b; a = b; b = c;}
+			return c;
+			};
+			}
+		var c = new fibonacci();
+		var fiboSeq = "";
+			for ( var i = 2 ; i <= 100 ; i++ ) {
+				fiboSeq += c.looping(i) + " ";}
 				console.log(fiboSeq)
-			function fibonacci() {
-			this.looping = function(nb){
-				var a = 0
-				var b = 1
-				var c = null;
-					for ( var i = 2; i <= nb; i++){
-						c = a + b; a = b; b = c;}
-					return c;
-					};
-					}
+			
